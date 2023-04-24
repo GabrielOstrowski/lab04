e14883b2edb7724411707e71a107637142d88fcf
GabrielOstrowski <gagiostr@gmail.com>
2023-04-24 15:48:03 +0200

    Kopiuje pliki i zmienia wartosc /c/Users/Gabon/bin:/mingw64/bin:/usr/local/bin:/usr/bin:/bin:/mingw64/bin:/usr/bin:/c/Users/Gabon/bin:/c/Program Files (x86)/Common Files/Intel/Shared Libraries/redist/intel64/compiler:/c/Program Files/Python38/Scripts:/c/Program Files/Python38:/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/c/ProgramData/Oracle/Java/javapath:/c/Windows/system32:/c/Windows:/c/Windows/System32/Wbem:/c/Windows/System32/WindowsPowerShell/v1.0:/c/Windows/System32/OpenSSH:/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/c/Program Files/NVIDIA Corporation/NVIDIA NvDLISR:/c/Program Files/dotnet:/c/WINDOWS/system32:/c/WINDOWS:/c/WINDOWS/System32/Wbem:/c/WINDOWS/System32/WindowsPowerShell/v1.0:/c/WINDOWS/System32/OpenSSH:/cmd:/c/Users/Gabon/AppData/Local/Microsoft/WindowsApps:/c/Program Files/JetBrains/PyCharm Community Edition 2022.3.1/bin:/usr/bin/vendor_perl:/usr/bin/core_perl

--- skrypt.sh
+++ skrypt.sh
@@ -4,12 +4,8 @@ licznik=1
 while [ $# -gt 0 ];
 do
   case "$1" in
-    -h|--help)
-	  echo "Uzycie: -d lub --date - pokazuje biezaca date"
-	  echo "-l lub --logs - domyslnie tworzy 100 plikow errorx.txt,"
-	  echo "podanie liczby w drugim argumencie powoduje"
-	  echo "utworzenie okreslonej liczby plikow errorx.txt"
-	  echo "-h lub --help - pokazuje pomoc"
-	  exit 0;;
+	-i|--init)
+	  cp *.* podkatalog/
+	  $PATH = C:/Users/$USER/podkatalog
   esac
 done
